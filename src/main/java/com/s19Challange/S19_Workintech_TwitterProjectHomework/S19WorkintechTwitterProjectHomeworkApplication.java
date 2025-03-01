0141e8b72231622f60906882a1f8560a6c4d98ce
Ali Umur Kucur <aliumurkucur@gmail.com>
2025-03-01 15:09:09 +0300

    Project's initial setup made

---
+++ src/main/java/com/s19Challange/S19_Workintech_TwitterProjectHomework/S19WorkintechTwitterProjectHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.s19Challange.S19_Workintech_TwitterProjectHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S19WorkintechTwitterProjectHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S19WorkintechTwitterProjectHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s19Challange/S19_Workintech_TwitterProjectHomework/S19WorkintechTwitterProjectHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s19Challange.S19_Workintech_TwitterProjectHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S19WorkintechTwitterProjectHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
